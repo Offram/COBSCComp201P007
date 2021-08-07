46b8a449e3ff8afe2c3727fdcd7c8c11b28ff8dc
Offram <sachinlagamuwa@gmail.com>
2021-08-07 13:35:06 +0530

    Added POST, PUT, DELETE access points

--- NodeJS Project/Project/app.js
+++ NodeJS Project/Project/app.js
@@ -11,13 +11,16 @@ let bearArray = [
   { id: 2, name: "Polar Bear", type: "Cute", place: "Polar Region" },
   { id: 3, name: "Sloth Bear", type: "Lazy", place: "All over" },
 ];
+
+app.use(express.json());
+//GET
 app.get("/", (req, res) => {
   res.send("Hello Everyone, Welcome to the class");
 });
 
+//GET ALL
 app.get("/api/bears", (req, res) => {
-  let bears = ["Grizzly Bear", "Polar Bear", "Sloth Bear"];
-  res.send(bears);
+  res.send(bearArray);
 });
 
 app.get("/api/bears/:bearId", (req, res) => {
@@ -27,8 +30,60 @@ app.get("/api/bears/:bearId", (req, res) => {
   let bear = bearArray.find((b) => {
     return b.id === bearId;
   });
+  if (!bear) {
+    return res.status(404).send("Given ID does not exist");
+  }
   res.send(bear);
 });
+
+//POST
+app.post("/api/bears", (req, res) => {
+
+  if (!req.body.name) {
+    return res.status(400).send("Not all mandatory values are set");
+  }
+  let newBearObj = {
+    id: bearArray.length + 1,
+    name: req.body.name,
+    type: req.body.type,
+    place: req.body.place
+  }
+  bearArray.push(newBearObj);
+  res.send(newBearObj);
+})
+
+//PUT
+app.put("/api/bears/:bearId", (req, res) => {
+  let bearId = parseInt(req.params.bearId);
+  let bear = bearArray.find((b) => {
+    return b.id === bearId;
+  });
+
+  if (!bear) {
+    return res.status(404).send("The given ID does not exist");
+  }
+
+  bear.name = req.body.name;
+  res.send(bear);
+})
+
+//DELETE
+app.delete("/api/bears/:bearId", (req, res) => {
+
+  let bearId = parseInt(req.params.bearId);
+  let bear = bearArray.find((b) => {
+    return b.id === bearId;
+  });
+
+  if (!bear) {
+    return res.status(404).send("The given ID does not exist");
+  }
+
+  let bearIndex = bearArray.indexOf(bear);
+  bearArray.splice(bearIndex, 1);
+
+  res.send(bear);
+})
 
 app.listen(3000, () => {
   console.log("Listen on Port 3000");
